228d5bbe77fedc92da963b7160756a435b99dd35
Banggi Bima <banggibima@gmail.com>
2024-12-03 17:00:44 +0700

    refactor: improve entity, repository, and service layers for device module

--- modules/device/domain/repository.go
+++ modules/device/domain/repository.go
@@ -1,1 +1,11 @@
 package domain
+
+type PostgresDeviceRepository interface {
+	CountAll() (int, error)
+	FindAll(offset, limit int, sort, order string, filters map[string]interface{}) ([]*Device, error)
+	FindByID(id int) (*Device, error)
+	Create(device *Device) error
+	Update(device *Device) error
+	UpdatePartial(device *Device) error
+	Delete(device *Device) error
+}
--- modules/device/domain/service.go
+++ modules/device/domain/service.go
@@ -1,1 +1,48 @@
 package domain
+
+import (
+	"github.com/banggibima/be-itam/pkg/config"
+)
+
+type DeviceService struct {
+	Config                   *config.Config
+	PostgresDeviceRepository PostgresDeviceRepository
+}
+
+func NewDeviceService(
+	config *config.Config,
+	postgresDeviceRepository PostgresDeviceRepository,
+) *DeviceService {
+	return &DeviceService{
+		Config:                   config,
+		PostgresDeviceRepository: postgresDeviceRepository,
+	}
+}
+
+func (s *DeviceService) CountAll() (int, error) {
+	return s.PostgresDeviceRepository.CountAll()
+}
+
+func (s *DeviceService) FindAll(offset, limit int, sort, order string, filters map[string]interface{}) ([]*Device, error) {
+	return s.PostgresDeviceRepository.FindAll(offset, limit, sort, order, filters)
+}
+
+func (s *DeviceService) FindByID(id int) (*Device, error) {
+	return s.PostgresDeviceRepository.FindByID(id)
+}
+
+func (s *DeviceService) Create(device *Device) error {
+	return s.PostgresDeviceRepository.Create(device)
+}
+
+func (s *DeviceService) Update(device *Device) error {
+	return s.PostgresDeviceRepository.Update(device)
+}
+
+func (s *DeviceService) UpdatePartial(device *Device) error {
+	return s.PostgresDeviceRepository.UpdatePartial(device)
+}
+
+func (s *DeviceService) Delete(device *Device) error {
+	return s.PostgresDeviceRepository.Delete(device)
+}
