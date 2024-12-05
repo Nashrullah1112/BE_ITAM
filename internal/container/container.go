9193db33cef29d275e66ecc02db39753980bf87b
Banggi Bima <banggibima@gmail.com>
2024-12-05 11:53:12 +0700

    refactor: update container.go for improved dependency injection

--- internal/container/container.go
+++ internal/container/container.go
@@ -7,11 +7,43 @@ import (
 	"github.com/banggibima/be-itam/internal/delivery/handlers"
 	"github.com/banggibima/be-itam/internal/delivery/middleware"
 	"github.com/banggibima/be-itam/internal/delivery/routes"
+	applicationcommand "github.com/banggibima/be-itam/modules/applications/application/command"
+	applicationquery "github.com/banggibima/be-itam/modules/applications/application/query"
+	applicationdomain "github.com/banggibima/be-itam/modules/applications/domain"
+	applicationrepository "github.com/banggibima/be-itam/modules/applications/infrastructure/repository"
+	assetcommand "github.com/banggibima/be-itam/modules/assets/application/command"
+	assetquery "github.com/banggibima/be-itam/modules/assets/application/query"
+	assetdomain "github.com/banggibima/be-itam/modules/assets/domain"
+	assetrepository "github.com/banggibima/be-itam/modules/assets/infrastructure/repository"
 	authcommand "github.com/banggibima/be-itam/modules/auth/application/command"
-	usercommand "github.com/banggibima/be-itam/modules/user/application/command"
-	userquery "github.com/banggibima/be-itam/modules/user/application/query"
-	userdomain "github.com/banggibima/be-itam/modules/user/domain"
-	userrepository "github.com/banggibima/be-itam/modules/user/infrastructure/repository"
+	devicecommand "github.com/banggibima/be-itam/modules/devices/application/command"
+	devicequery "github.com/banggibima/be-itam/modules/devices/application/query"
+	devicedomain "github.com/banggibima/be-itam/modules/devices/domain"
+	devicerepository "github.com/banggibima/be-itam/modules/devices/infrastructure/repository"
+	divisioncommand "github.com/banggibima/be-itam/modules/divisions/application/command"
+	divisionquery "github.com/banggibima/be-itam/modules/divisions/application/query"
+	divisiondomain "github.com/banggibima/be-itam/modules/divisions/domain"
+	divisionrepository "github.com/banggibima/be-itam/modules/divisions/infrastructure/repository"
+	hardwarecommand "github.com/banggibima/be-itam/modules/hardwares/application/command"
+	hardwarequery "github.com/banggibima/be-itam/modules/hardwares/application/query"
+	hardwaredomain "github.com/banggibima/be-itam/modules/hardwares/domain"
+	hardwarerepository "github.com/banggibima/be-itam/modules/hardwares/infrastructure/repository"
+	licensecommand "github.com/banggibima/be-itam/modules/licenses/application/command"
+	licensequery "github.com/banggibima/be-itam/modules/licenses/application/query"
+	licensedomain "github.com/banggibima/be-itam/modules/licenses/domain"
+	licenserepository "github.com/banggibima/be-itam/modules/licenses/infrastructure/repository"
+	positioncommand "github.com/banggibima/be-itam/modules/positions/application/command"
+	positionquery "github.com/banggibima/be-itam/modules/positions/application/query"
+	positiondomain "github.com/banggibima/be-itam/modules/positions/domain"
+	positionrepository "github.com/banggibima/be-itam/modules/positions/infrastructure/repository"
+	usercommand "github.com/banggibima/be-itam/modules/users/application/command"
+	userquery "github.com/banggibima/be-itam/modules/users/application/query"
+	userdomain "github.com/banggibima/be-itam/modules/users/domain"
+	userrepository "github.com/banggibima/be-itam/modules/users/infrastructure/repository"
+	vendorcommand "github.com/banggibima/be-itam/modules/vendors/application/command"
+	vendorquery "github.com/banggibima/be-itam/modules/vendors/application/query"
+	vendordomain "github.com/banggibima/be-itam/modules/vendors/domain"
+	vendorrepository "github.com/banggibima/be-itam/modules/vendors/infrastructure/repository"
 	"github.com/banggibima/be-itam/pkg/config"
 	"github.com/gofiber/fiber/v2"
 	"github.com/minio/minio-go/v7"
@@ -43,11 +75,59 @@ func NewContainer(
 }
 
 func (c *Container) Setup() error {
-	postgresUserRepository := userrepository.NewPostgresUserRepository(c.Config, c.DB, c.Logger)
-	userService := userdomain.NewUserService(c.Config, postgresUserRepository)
+	divisionPostgresRepository := divisionrepository.NewPostgresDivisionRepository(c.Config, c.DB, c.Logger)
+	divisionService := divisiondomain.NewDivisionService(c.Config, divisionPostgresRepository)
+	divisionCommandUsecase := divisioncommand.NewDivisionCommandUsecase(c.Config, divisionService)
+	divisionQueryUsecase := divisionquery.NewDivisionQueryUsecase(c.Config, divisionService)
+	divisionHandler := handlers.NewDivisionHandler(c.Config, divisionCommandUsecase, divisionQueryUsecase)
+
+	positionPostgresRepository := positionrepository.NewPostgresPositionRepository(c.Config, c.DB, c.Logger)
+	positionService := positiondomain.NewPositionService(c.Config, positionPostgresRepository)
+	positionCommandUsecase := positioncommand.NewPositionCommandUsecase(c.Config, positionService)
+	positionQueryUsecase := positionquery.NewPositionQueryUsecase(c.Config, positionService)
+	positionHandler := handlers.NewPositionHandler(c.Config, positionCommandUsecase, positionQueryUsecase)
+
+	vendorPostgresRepository := vendorrepository.NewPostgresVendorRepository(c.Config, c.DB, c.Logger)
+	vendorService := vendordomain.NewVendorService(c.Config, vendorPostgresRepository)
+	vendorCommandUsecase := vendorcommand.NewVendorCommandUsecase(c.Config, vendorService)
+	vendorQueryUsecase := vendorquery.NewVendorQueryUsecase(c.Config, vendorService)
+	vendorHandler := handlers.NewVendorHandler(c.Config, vendorCommandUsecase, vendorQueryUsecase)
+
+	assetPostgresRepository := assetrepository.NewPostgresAssetRepository(c.Config, c.DB, c.Logger)
+	assetService := assetdomain.NewAssetService(c.Config, assetPostgresRepository)
+	assetCommandUsecase := assetcommand.NewAssetCommandUsecase(c.Config, assetService)
+	assetQueryUsecase := assetquery.NewAssetQueryUsecase(c.Config, assetService)
+	assetHandler := handlers.NewAssetHandler(c.Config, assetCommandUsecase, assetQueryUsecase)
+
+	applicationPostgresRepository := applicationrepository.NewPostgresApplicationRepository(c.Config, c.DB, c.Logger)
+	applicationService := applicationdomain.NewApplicationService(c.Config, applicationPostgresRepository)
+	applicationCommandUsecase := applicationcommand.NewApplicationCommandUsecase(c.Config, applicationService)
+	applicationQueryUsecase := applicationquery.NewApplicationQueryUsecase(c.Config, applicationService)
+	applicationHandler := handlers.NewApplicationHandler(c.Config, applicationCommandUsecase, applicationQueryUsecase)
+
+	userPostgresRepository := userrepository.NewPostgresUserRepository(c.Config, c.DB, c.Logger)
+	userService := userdomain.NewUserService(c.Config, userPostgresRepository)
 	userCommandUsecase := usercommand.NewUserCommandUsecase(c.Config, userService)
 	userQueryUsecase := userquery.NewUserQueryUsecase(c.Config, userService)
 	userHandler := handlers.NewUserHandler(c.Config, userCommandUsecase, userQueryUsecase)
+
+	devicePostgresRepository := devicerepository.NewPostgresDeviceRepository(c.Config, c.DB, c.Logger)
+	deviceService := devicedomain.NewDeviceService(c.Config, devicePostgresRepository)
+	deviceCommandUsecase := devicecommand.NewDeviceCommandUsecase(c.Config, deviceService)
+	deviceQueryUsecase := devicequery.NewDeviceQueryUsecase(c.Config, deviceService)
+	deviceHandler := handlers.NewDeviceHandler(c.Config, deviceCommandUsecase, deviceQueryUsecase)
+
+	hardwarePostgresRepository := hardwarerepository.NewPostgresHardwareRepository(c.Config, c.DB, c.Logger)
+	hardwareService := hardwaredomain.NewHardwareService(c.Config, hardwarePostgresRepository)
+	hardwareCommandUsecase := hardwarecommand.NewHardwareCommandUsecase(c.Config, hardwareService)
+	hardwareQueryUsecase := hardwarequery.NewHardwareQueryUsecase(c.Config, hardwareService)
+	hardwareHandler := handlers.NewHardwareHandler(c.Config, hardwareCommandUsecase, hardwareQueryUsecase)
+
+	licensePostgresRepository := licenserepository.NewPostgresLicenseRepository(c.Config, c.DB, c.Logger)
+	licenseService := licensedomain.NewLicenseService(c.Config, licensePostgresRepository)
+	licenseCommandUsecase := licensecommand.NewLicenseCommandUsecase(c.Config, licenseService)
+	licenseQueryUsecase := licensequery.NewLicenseQueryUsecase(c.Config, licenseService)
+	licenseHandler := handlers.NewLicenseHandler(c.Config, licenseCommandUsecase, licenseQueryUsecase)
 
 	authCommandUsecase := authcommand.NewAuthCommandUsecase(c.Config, userService)
 	authHandler := handlers.NewAuthHandler(c.Config, authCommandUsecase)
@@ -60,10 +140,18 @@ func (c *Container) Setup() error {
 	c.App.Use(loggerMiddleware.Logging())
 
 	routes := &routes.Routes{
-		App:           c.App,
-		UserHandler:   userHandler,
-		AuthHandler:   authHandler,
-		JWTMiddleware: jwtMiddleware,
+		App:                c.App,
+		DivisionHandler:    divisionHandler,
+		PositionHandler:    positionHandler,
+		VendorHandler:      vendorHandler,
+		AssetHandler:       assetHandler,
+		ApplicationHandler: applicationHandler,
+		UserHandler:        userHandler,
+		DeviceHandler:      deviceHandler,
+		HardwareHandler:    hardwareHandler,
+		LicenseHandler:     licenseHandler,
+		AuthHandler:        authHandler,
+		JWTMiddleware:      jwtMiddleware,
 	}
 
 	routes.Resource()
