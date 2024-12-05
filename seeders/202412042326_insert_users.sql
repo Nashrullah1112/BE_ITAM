5b316355ae5712760a6113d5f23edf9e969f0a54
Banggi Bima <banggibima@gmail.com>
2024-12-05 12:02:19 +0700

    feat: add initial user seeder script 202412042326_insert_users.sql

---
+++ seeders/202412042326_insert_users.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (nip, name, email, password, role) 
+VALUES 
+  ('1234567890', 'Muhammad Navis Nashrullah', 'navis@mail.com', '$2a$12$JBTkxOGi/zvVd7EgCYYa0eSznprBzXpzyMX/eIrnBJ9oZuMCywvYS', 'admin'),
+  ('0987654321', 'Muhammad Navis Nashrullah', 'navissupervisor@mail.com', '$2a$12$XbU0LxrVwYzUG.Hvq7KDr.TZYxrnOPcZLmQRsEfjiHpEJf.Mnn5sy', 'supervisor'),
+  ('0987654322', 'Muhammad Navis Nashrullah', 'navisuser@mail.com', '$2a$12$6nopF3hTgouP1yNuU/jfVu2OP81KdLEnQ8jenp/JXDxJx4FyA3a/y', 'user');
